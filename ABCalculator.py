b4dba85e43811787599517580c299f3ea14e04b1
Costa <deysler@yandex.ru>
2023-02-17 21:27:56 +0300

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,13 @@
+# A/B калькулятор
+
+import tkinter as tk
+
+# Создание шлавного окна
+root = tk.Tk()
+root.geometry("280x300")
+root.title("A/B калькулятор")
+
+# Запуск цикла mainloop
+root.mainloop()
+
+
